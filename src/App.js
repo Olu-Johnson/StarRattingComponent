92f497287b1c15129eda41e913ed9d393af2f575
Olu-Johnson Oluwatosin <olujohnsonoluwatobi@gmail.com>
2024-02-21 06:05:28 -0330

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import {FaStar} from 'react-icons/fa'
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [rating, setRating]  = useState(null)
+  const [hover, setHover] =  useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" style={{paddingTop:'30px'}}>
+        {[...Array(5)].map((star, index) =>{
+          const currentRating = index + 1;
+          return (
+            <label>
+              <input 
+                type="radio"
+                name="rating"
+                value = {currentRating}
+                onClick={() => setRating(currentRating)}
+                />
+                <FaStar className='star' size={50} 
+                 color={currentRating <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+                 onMouseEnter={() => setHover(currentRating)}
+                 onMouseLeave={() => setHover(null)}
+                />
+            </label>
+          ) 
+        })
+      }
+      <p>Your rating is {rating}</p>
+     </div>
   );
 }
 
